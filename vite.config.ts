26e9e4ff469b8df506a95446c1190cf4e63f60c4
momosuke <rusk.ryoy4n@gmail.com>
2023-10-05 15:55:51 +0900

    add: icons

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,16 @@ const manifest = defineManifest({
   manifest_version: 3,
   name: "PYTV - Post YouTube Video",
   description: "Extension to post YouTube video to X (formerly Twitter)",
+  icons: {
+    128: "icons/icon-128px.png",
+    48: "icons/icon-48px.png",
+    16: "icons/icon-16px.png",
+  },
   version: "1.0.0",
   permissions: ["activeTab"],
   action: {
     default_popup: "index.html",
+    default_icon: "icons/icon-128px.png"
   },
   background: {
     service_worker: "src//background/index.ts",
