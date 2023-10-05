2daf6200d6dde1a160c4632e9329833b776729cb
momosuke <rusk.ryoy4n@gmail.com>
2023-10-05 16:57:05 +0900

    delete: unrequired code

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import { api_endpoint } from '../../endpoint.json'
 import { FaCheckCircle, FaInfoCircle } from 'react-icons/fa';
@@ -42,37 +41,6 @@ function App(): JSX.Element {
 
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header> */}
       <h1 className="App-name">PYTV</h1>
       <p className="title__text">Title</p>
       {/* <p className="title__value">{title}</p> */}
